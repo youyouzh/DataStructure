13da42d3263ebf73a3c9d5a43ece2add59225080
youyoui <1406558940@qq.com>
2018-05-31 22:59:13 +0800

    search

---
+++ DataStructure/u_search.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <vector>
+
+using namespace std;
+
+// 各种查找算法
+template<class T>
+int sequentialSearch(T a[], int n, const T& x)
+{
+	// 顺序查找数组a中值为x的位置
+	int i;
+	for (i = 0; i < n && a[i] != x; i++);
+	if (i == n) {
+		return -1;
+	}
+	else {
+		return i;
+	}
+}
